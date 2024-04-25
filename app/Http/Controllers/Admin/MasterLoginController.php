bf075f5768b1a2285af378b15293efab13e931cb
ali.cyber <muhammad.ali@cyberonix.org>
2024-04-25 06:12:18 +0500

    working on master login feature

--- app/Http/Controllers/Admin/MasterLoginController.php
+++ app/Http/Controllers/Admin/MasterLoginController.php
@@ -11,12 +11,7 @@ class MasterLoginController extends Controller
 {
     public function login(Request $request, $company_id)
     {
-
-
         try {
-
-
-            
             $company_id = base64_decode($company_id);
             $baseUrl = findBaseUrl($company_id);
             $client = new Client([
@@ -31,19 +26,15 @@ class MasterLoginController extends Controller
                 'form_params' => $params,
             ]);
             $body = $response->getBody()->getContents();
-
             $responseData = json_decode($body, true);
+            dd($responseData);
         } catch (RequestException $e) {
-            // Handle request exceptions (e.g., network errors, HTTP errors)
+
             if ($e->hasResponse()) {
-                // Get the HTTP response status code and message
                 $statusCode = $e->getResponse()->getStatusCode();
                 $reasonPhrase = $e->getResponse()->getReasonPhrase();
-
-                // Handle the error based on the status code and reason phrase
                 echo "Request failed with status code $statusCode: $reasonPhrase";
             } else {
-                // Handle other types of request exceptions (e.g., connection timeout)
                 echo "Request failed: " . $e->getMessage();
             }
         }
