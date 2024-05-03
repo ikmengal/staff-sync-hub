b4a46617c83488aa20cc6d53c0addb6fa5574fbc
DESKTOP-38C2NEK\Haseeb Hassan <mhhassan095@gmail.com>
2024-05-03 15:49:32 -0700

    error pages

--- resources/views/errors/409.blade.php
+++ resources/views/errors/409.blade.php
@@ -3,8 +3,8 @@
     <!-- Content -->
     <div class="container-xxl container-p-y">
       <div class="misc-wrapper">
-        <h2 class="mb-1 mt-4">Page Not Found :(</h2>
-        <p class="mb-4 mx-2">Oops! 😖 The requested URL was not found on this server.</p>
+        <h2 class="mb-1 mt-4">Your account is suspended</h2>
+        <p class="mb-4 mx-2">The page you are trying to access is not available.</p>
         <a href="{{ route('dashboard') }}" class="btn btn-primary mb-4">Back to home</a>
         <div class="mt-4">
           <img
