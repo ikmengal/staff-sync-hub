d3b2369b229e07d413904cae32fc1fb9fbf5e744
DESKTOP-38C2NEK\Haseeb Hassan <mhhassan095@gmail.com>
2024-05-03 14:11:45 -0700

    fixes

--- resources/views/admin/permissions/permissions.blade.php
+++ resources/views/admin/permissions/permissions.blade.php
@@ -1,5 +1,5 @@
 @foreach($permission as $value)
   
-<span class="badge bg-label-success" data-bs-toggle="tooltip" title="{{$value->name}}">{{ucfirst($value->display_name)}}</span>
+<span class="badge bg-label-success mt-1" data-bs-toggle="tooltip" title="{{$value->name}}">{{ucfirst($value->display_name)}}</span>
  
 @endforeach
