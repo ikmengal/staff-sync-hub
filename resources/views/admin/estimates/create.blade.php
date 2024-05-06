da9b4862fa46668515777e32cea17f591c2058f5
DESKTOP-2S938QD\User-1 <imran.ali@cyberonix.org>
2024-05-07 00:03:12 +0500

    Purchase Request work completed

--- resources/views/admin/estimates/create.blade.php
+++ resources/views/admin/estimates/create.blade.php
@@ -21,9 +21,9 @@
                     <form action="{{route('estimates.store')}}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="row">
-                            <div class="col-md-6 mb-5">
-                                <label for="">Purchase Request </label>
-                                <select name="request_id" class="form-control" id="" required>
+                            <div class="col-md-4 mb-5">
+                                <label for="">Purchase Request <span class="text-danger">*</span></label>
+                                <select name="request_id" class="form-control" id="request_id">
                                     <option value="">--------</option>
                                     @if(isset($requests) && !empty($requests))
                                     @foreach($requests as $index => $value)
@@ -32,8 +32,11 @@
                                     @endif
                                 </select>
                                 <small>Leave this drop down empty if you wanna create an estimate on the basis of company</small>
+                                @error('request_id')
+                                    <span class="text-danger error" id="request_id_error" >{{ $message ?? '' }}</span>
+                                @enderror
                             </div>
-                            <div class="col-md-6 mb-5">
+                            {{-- <div class="col-md-6 mb-5">
                                 <label for="">Company <span class="text-danger">*</span></label>
                                 <select name="company_id" class="form-control" id="">
                                     <option value="">--------</option>
@@ -45,18 +48,27 @@
                                 </select>
                                 <!-- <input type="text" name="" id="company_name" class="form-control " readonly placeholder="Select Purchase Request First">
                                 <input type="hidden" name="company_id" id=""> -->
-                            </div>
-                            <div class="col-md-6 mb-5">
+                            </div> --}}
+                            <div class="col-md-4 mb-5">
                                 <label for="">Title <span class="text-danger">*</span></label>
                                 <input type="text" name="title" id="title" class="form-control " placeholder="Title of estimate">
+                                @error('title')
+                                    <span class="text-danger error" id="title_error" >{{ $message ?? '' }}</span>
+                                @enderror
                             </div>
-                            <div class="col-md-6 mb-5">
+                            <div class="col-md-4 mb-5">
                                 <label for="">Price <span class="text-danger">*</span></label>
                                 <input type="text" name="price" id="price" class="form-control " placeholder="Estimated Price">
+                                @error('price')
+                                    <span class="text-danger error" id="price_error" >{{ $message ?? '' }}</span>
+                                @enderror
                             </div>
                             <div class="col-md-12 mb-5">
-                                <label for="">Description</label>
-                                <textarea name="description" id="" cols="30" rows="10" class="form-control" placeholder="Enter description"></textarea>
+                                <label for="">Description <span class="text-danger">*</span></label>
+                                <textarea name="description" id="description" cols="30" rows="10" class="form-control" placeholder="Enter description"></textarea>
+                                @error('description')
+                                    <span class="text-danger error" id="description_error" >{{ $message ?? '' }}</span>
+                                @enderror
                             </div>
                             <div class="col-md-12 mb-5">
                                 <label for="">Attachments</label>
