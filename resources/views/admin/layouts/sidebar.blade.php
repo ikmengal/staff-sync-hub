1a5b91b9fa1e3b491dfeca03d10c057ff1cc6314
ali.cyber <muhammad.ali@cyberonix.org>
2024-05-17 22:47:58 +0500

    permission name on side bar changed

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -146,7 +146,7 @@
         </li>
         @endcan
         <li class="menu-item {{ request()->is('admin/user_leaves')?'active':'' }}">
-            @can('leave-types-list')
+            @can('user-leaves-list')
             <a href="{{ route('user_leaves.index') }}" class="menu-link">
                 <i class="menu-icon tf-icons ti ti-clock"></i>
                 <div>All Leaves</div>
